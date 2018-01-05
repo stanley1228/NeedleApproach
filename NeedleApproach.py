1f29ec94e9c262cd5f22fb26677d256dab27c091
stanley1228 <stanleyfreeze@gmail.com>
2018-01-05 17:02:05 +0800

    刪掉註解

--- NeedleApproach.py
+++ NeedleApproach.py
@@ -1,31 +1,15 @@
 import numpy as np
 import cv2
 
-
-'''
-add optical flow
-'''
-
 cap = cv2.VideoCapture(0)
 
 ret,img1 =cap.read()
-
-#cv2.imshow("img1",img1)
-#cv2.waitKey(1)
 
 gray1 = cv2.cvtColor(img1,cv2.COLOR_BGR2GRAY)
 gray1 = np.float32(gray1)
 Hcorner1 = cv2.cornerHarris(gray1,blockSize=2,ksize=3,k=0.05)
  
 #dilated for marking the corners, not important
-kernel = np.ones((8,8), np.uint8) 
-Hcorner1 = cv2.dilate(Hcorner1,kernel,iterations=1)
-
-#harris_result=img1.copy()
-#harris_result[Hcorner1>0.2*Hcorner1.max()]=[0,0,255]  
-#cv2.imshow('harris',harris_result)
-#cv2.waitKey(1)
-
 feature_params = dict( maxCorners = 20,
                     qualityLevel = 0.2,
                     minDistance = 40,
@@ -34,20 +18,16 @@ feature_params = dict( maxCorners = 20,
                     k=0.05)
 
 gray1 = cv2.cvtColor(img1,cv2.COLOR_BGR2GRAY)
-#GFeature1 = cv2.goodFeaturesToTrack(gray1,25,0.01,10)
 GFeature1 = cv2.goodFeaturesToTrack(gray1,**feature_params)
-
 goodFeature_result=img1.copy()
 
 for i in GFeature1:
     x,y = i.ravel()
-    cv2.circle(goodFeature_result,(x,y),3,255,-1)
+    cv2.circle(goodFeature_result,(x,y),3,255,-1)#plot circle
 
-#cv2.imshow('first_get',goodFeature_result)
-#cv2.waitKey(1)
-
-
-#find needle point 
+#=================
+#find needle point
+#================= 
 NeedleGet=False
 while(True):   
     ret,img2=cap.read()
@@ -57,7 +37,8 @@ while(True):
     lk_params = dict( winSize  = (15,15), maxLevel = 2, criteria = (cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 0.03))
     FlowPoint1, status, err=cv2.calcOpticalFlowPyrLK(gray1,gray2,GFeature1,None,**lk_params)
 
-    # Select good points ==> status is 1 and moving a enough distnace  
+    #Find a point that move the most far from last frame and current frame
+    #the point is needle point
     runtime=len(FlowPoint1)
     dist=[]
     for i in range(runtime):
@@ -69,13 +50,12 @@ while(True):
     max_index=dist.index(max_dist)
     if status[max_index]==1 and (max_dist>20):
         ss="distance={:.3f}".format(max_dist)
-        #print(ss)
      
         x,y=FlowPoint1[max_index].ravel()
         NeedlePoint=(x,y)
         NeedleGet=True
         ss="Needle={:.3f},{:.3f}".format(NeedlePoint[0],NeedlePoint[1])
-        #print(ss)
+     
         if len(FlowPoint1)>1:
             GFeature1=FlowPoint1[max_index]
         else:
@@ -94,18 +74,17 @@ while(True):
     cv2.imshow('goodFeature1',goodFeature_result)    
     cv2.waitKey(100)
 
+    #record the needle coordinate
     if NeedleGet==True:
         NeedlePoint=(x,y)
 
-    #record the needle coordinate and break this loop
+    #wait space key to change to find the symbol 
     if (cv2.waitKey(100) & 0xFF == ord(' ')):
         print('start symbol detect')
         cv2.destroyWindow('goodFeature1')
         cv2.waitKey(100)
         break
 
- 
-    
 ret,img1 =cap.read()
 feature_params = dict( maxCorners = 20,qualityLevel = 0.1, minDistance = 50,blockSize = 2,useHarrisDetector=False,k=0.05)
 gray1 = cv2.cvtColor(img1,cv2.COLOR_BGR2GRAY)
@@ -118,8 +97,9 @@ for i in GFeature2:
 cv2.imshow('first_get',goodFeature_result)
 cv2.waitKey(1)
 
-        
+#=========================
 #find the symbol on object
+#=========================
 SymbolGet=False
 while(True):
     ret,img2=cap.read()
@@ -129,7 +109,8 @@ while(True):
     lk_params = dict( winSize  = (15,15), maxLevel = 2, criteria = (cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 0.03))
     FlowPoint2, status, err=cv2.calcOpticalFlowPyrLK(gray1,gray2,GFeature2,None,**lk_params)
 
-    # Select good points ==> status is 1 and moving a enough distnace  
+    #Find a point that move the most far from last frame and current frame
+    #the point is needle point
     runtime=len(FlowPoint2)
     dist=[]
     for i in range(runtime):
@@ -150,7 +131,8 @@ while(True):
             GFeature2=FlowPoint2.copy()
         gray1=gray2.copy()
 
-
+    #check the offset between needle point and object
+    #and give the direction hint
     if SymbolGet==True:
         SymbolPoint=(x,y)
  
@@ -170,7 +152,7 @@ while(True):
         if(offset_x<20) and (offset_y<20):
             ss='ok!'
         
-    #show tracking feature
+    #show point pixel on the image
     match_symbol=img2.copy()
     cv2.circle(match_symbol,NeedlePoint,3,(0,0,255),-1)
     if SymbolGet==True:
@@ -188,89 +170,3 @@ while(True):
 
     if SymbolGet==True:
         SymbolPoint=(x,y)
-
-
-    
-
-    
- 
-#
-#harris corner
-#
-'''
-cap = cv2.VideoCapture(0)
-while(True):
-    ret, img = cap.read()
-
-    print(img.shape)
-    #filename = 'Needle2.jpg'
-    #img = cv2.imread(filename)
-    #print(img.shape)
-
-    cv2.namedWindow('Needle')
-    cv2.imshow("Needle",img)
-
-    gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-
-    gray = np.float32(gray)
-    dst = cv2.cornerHarris(gray,blockSize=2,ksize=3,k=0.05)
-   
-    #result is dilated for marking the corners, not important
-    #dst = cv2.dilate(dst,None)
-    kernel = np.ones((8,8), np.uint8)
-    dst = cv2.dilate(dst,kernel,iterations=1)
-
-    # Threshold for an optimal value, it may vary depending on the image.
-    #the needle corner score is too low 
-    #original is 0.01*dst.max()  now change to 0.001*dst.max() then needle can be found
-    #i think it cause by the needle and the sewing machine gray part has similar color
-    img[dst>0.1*dst.max()]=[0,0,255]  
-    cv2.imshow('harris',dst)
-    cv2.imshow('dst',img)
-
-    #if cv2.waitKey(0) & 0xff == 27:
-    #    cv2.destroyAllWindows()
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-'''
-
-'''
-filename = 'Needle2.jpg'
-img = cv2.imread(filename)
-print(img.shape)
-cv2.imshow("Needle",img)
-gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-gray = np.float32(gray)
-corners = cv2.goodFeaturesToTrack(gray,25,0.01,10)
-corners = np.int0(corners)
-for i in corners:
-    x,y = i.ravel()
-    cv2.circle(img,(x,y),3,255,-1)
-cv2.imshow("dst",img)
-if cv2.waitKey(0) & 0xff == 27:
-    cv2.destroyAllWindows()
-'''
-
-
-#
-#video
-#
-'''
-cap = cv2.VideoCapture(0)
-while(True):
-    # Capture frame-by-frame
-    ret, frame = cap.read()
-    #height,width = frame.shape
-    
-    print(frame.shape)
-    # Our operations on the frame come here
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    # Display the resulting frame
-    cv2.imshow('frame',gray)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-# When everything done, release the capture
-cap.release()
-cv2.destroyAllWindows()
-print('Hello World')
-'''
